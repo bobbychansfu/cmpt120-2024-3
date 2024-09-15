7c4988158dde6d6d2e24669be926cc905d7bddd5
bobby chan <bobbyc@bobbys-MacBook-Pro.local>
2024-09-15 11:20:57 -0700

    topic2

---
+++ 1/addition.py
@@ -0,0 +1,19 @@
+'''
+adds two number and displays result
+input - a,b integers
+output - a+b
+sample:
+2
+3
+output: 5
+'''
+
+# todo: ask user for a,b
+# ask user for input, put it into a
+a_int = int(input("Please enter an integer: "))
+# ask user for input, put it into b
+b_int = int(input("Please enter an integer: "))
+# add a+b
+sum_of_ab = a_int + b_int
+# display result
+print(sum_of_ab)
---
+++ 1/coercion.py
@@ -0,0 +1,4 @@
+print(5+5.01)
+print(5 / 3)
+print(5.4//3)
+print("hello"*3)
